5cb8e775e64f2177ba40d01ed7e5d9af46978c78
Kristián Feldsam <feldsam@gmail.com>
2018-06-29 14:16:39 +0200

    Reverting exchange rates changes
    Signed-off-by: Kristián Feldsam <feldsam@gmail.com>

--- app/Console/Commands/SendReminders.php
+++ app/Console/Commands/SendReminders.php
@@ -255,7 +255,7 @@ class SendReminders extends Command
 
                 foreach ($data->rates as $code => $rate) {
                     if($recalculate) {
-                        $rate = $data->rates->{$base} * 1 / $rate;
+                        $rate = 1 / $data->rates->{$base} * $rate;
                     }
 
                     Currency::whereCode($code)->update(['exchange_rate' => $rate]);
