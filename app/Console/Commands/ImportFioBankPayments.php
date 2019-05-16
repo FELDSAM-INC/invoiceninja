5bcec3a4c7a31c6c6010c1a8280d8548abffa331
Kristián Feldsam <feldsam@gmail.com>
2019-05-16 14:24:13 +0200

    ImportFioBankPayments skip one time payment
    Signed-off-by: Kristián Feldsam <feldsam@gmail.com>

--- app/Console/Commands/ImportFioBankPayments.php
+++ app/Console/Commands/ImportFioBankPayments.php
@@ -166,6 +166,7 @@ class ImportFioBankPayments extends Command
         if($transaction->getId() == '17694406216') return null;
         if($transaction->getId() == '17724783278') return null;
         if($transaction->getId() == '20807288786') return null;
+        if($transaction->getId() == '21328846473') return null;
 
         // check if payment already exists
         if($payment = Payment::where('private_notes', $hash)->where('is_deleted', '!=', 1)->first())
